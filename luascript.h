39bb6fc3bf0fc9f8777eae66c1ae4433703630b5
Paulo H Perbone Rocha <paulohenriquerocha@yahoo.com>
2017-11-07 15:02:20 -0200

    Add base resource extension

--- luascript.h
+++ luascript.h
@@ -1,12 +1,14 @@
 
 #pragma once
 
+#include "io/resource_loader.h"
+#include "io/resource_saver.h"
 #include "script_language.h"
 
 
 class LuaScript : public Script {
 	GDCLASS(LuaScript, Script)
-
+	RES_BASE_EXTENSION("lua");
 protected:
 
 	static void _bind_methods();
