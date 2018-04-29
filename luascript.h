198d85e7be887deadc5ecbf5af171d00281982b9
Paulo H Perbone Rocha <paulohenriquerocha@yahoo.com>
2018-04-28 23:06:43 -0300

    Add missing copyright and license header + initial wip register/unregister methods

--- luascript.h
+++ luascript.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of LuaScript
+ * https://github.com/perbone/luascrip/
+ *
+ * Copyright 2017-2018 Paulo Perbone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not  use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 
 #pragma once
 
@@ -5,10 +23,187 @@
 #include "io/resource_saver.h"
 #include "script_language.h"
 
-class LuaScript : public Script {
-	GDCLASS(LuaScript, Script)
-	RES_BASE_EXTENSION("lua");
+class LuaScript: public Script {
+
+    GDCLASS(LuaScript, Script)
+
+	friend class LuaScriptInstance;
+	friend class LuaScriptLanguage;
+
+public:
+
+    LuaScript();
+    ~LuaScript();
+
+    virtual bool can_instance() const;
+
+    virtual Ref<Script> get_base_script() const;
+
+    virtual StringName get_instance_base_type() const;
+    virtual ScriptInstance *instance_create(Object *p_this);
+    virtual bool instance_has(const Object *p_this) const;
+
+    virtual bool has_source_code() const;
+    virtual String get_source_code() const;
+    virtual void set_source_code(const String &p_code);
+    virtual Error reload(bool p_keep_state = false);
+
+    virtual bool has_method(const StringName &p_method) const;
+    virtual MethodInfo get_method_info(const StringName &p_method) const;
+
+    virtual bool is_tool() const;
+
+    virtual ScriptLanguage *get_language() const;
+
+    virtual bool has_script_signal(const StringName &p_signal) const;
+    virtual void get_script_signal_list(List<MethodInfo> *r_signals) const;
+
+    virtual bool get_property_default_value(const StringName &p_property, Variant &r_value) const;
+
+    virtual void update_exports();
+    virtual void get_script_method_list(List<MethodInfo> *p_list) const;
+    virtual void get_script_property_list(List<PropertyInfo> *p_list) const;
+
+    virtual int get_member_line(const StringName &p_member) const;
+
+    virtual void get_constants(Map<StringName, Variant> *p_constants);
+    virtual void get_members(Set<StringName> *p_constants);
 
 protected:
-	static void _bind_methods();
+
+    static void _bind_methods();
+
+private:
+
+};
+
+class LuaScriptInstance: public ScriptInstance {
+
+	friend class LuaScript;
+
+public:
+
+	LuaScriptInstance();
+	~LuaScriptInstance();
+
+    virtual bool set(const StringName &p_name, const Variant &p_value);
+    virtual bool get(const StringName &p_name, Variant &r_ret) const;
+    virtual void get_property_list(List<PropertyInfo> *p_properties) const;
+    virtual Variant::Type get_property_type(const StringName &p_name, bool *r_is_valid = NULL) const;
+
+    virtual Object *get_owner();
+    virtual void get_property_state(List<Pair<StringName, Variant> > &state);
+
+    virtual void get_method_list(List<MethodInfo> *p_list) const;
+    virtual bool has_method(const StringName &p_method) const;
+    virtual Variant call(const StringName &p_method, VARIANT_ARG_LIST);
+    virtual Variant call(const StringName &p_method, const Variant **p_args, int p_argcount, Variant::CallError &r_error);
+    virtual void call_multilevel(const StringName &p_method, VARIANT_ARG_LIST);
+    virtual void call_multilevel(const StringName &p_method, const Variant **p_args, int p_argcount);
+    virtual void call_multilevel_reversed(const StringName &p_method, const Variant **p_args, int p_argcount);
+    virtual void notification(int p_notification);
+
+    virtual void refcount_incremented();
+    virtual bool refcount_decremented();
+
+    virtual Ref<Script> get_script() const;
+
+    virtual bool is_placeholder();
+
+    virtual RPCMode get_rpc_mode(const StringName &p_method) const;
+    virtual RPCMode get_rset_mode(const StringName &p_variable) const;
+
+    virtual ScriptLanguage *get_language();
+    virtual ~ScriptInstance();
+
+protected:
+
+    static void _bind_methods();
+
+private:
+
+};
+
+class LuaScriptLanguage: public ScriptLanguage {
+
+	friend class LuaScript;
+	friend class LuaScriptInstance;
+
+public:
+
+	LuaScriptLanguage();
+	~LuaScriptLanguage();
+
+    virtual String get_name() const;
+
+    virtual void init();
+    virtual String get_type() const;
+    virtual String get_extension() const;
+    virtual Error execute_file(const String &p_path);
+    virtual void finish();
+
+    virtual void get_reserved_words(List<String> *p_words) const;
+    virtual void get_comment_delimiters(List<String> *p_delimiters) const;
+    virtual void get_string_delimiters(List<String> *p_delimiters) const;
+    virtual Ref<Script> get_template(const String &p_class_name, const String &p_base_class_name) const;
+    virtual void make_template(const String &p_class_name, const String &p_base_class_name, Ref<Script> &p_script) {}
+    virtual bool is_using_templates() { return false; }
+    virtual bool validate(const String &p_script, int &r_line_error, int &r_col_error, String &r_test_error, const String &p_path = "", List<String> *r_functions = NULL) const;
+    virtual String validate_path(const String &p_path) const { return ""; }
+    virtual Script *create_script() const;
+    virtual bool has_named_classes() const;
+    virtual bool supports_builtin_mode() const;
+    virtual bool can_inherit_from_file() { return false; }
+    virtual int find_function(const String &p_function, const String &p_code) const;
+    virtual String make_function(const String &p_class, const String &p_name, const PoolStringArray &p_args) const;
+    virtual Error open_in_external_editor(const Ref<Script> &p_script, int p_line, int p_col) { return ERR_UNAVAILABLE; }
+    virtual bool overrides_external_editor() { return false; }
+
+    virtual Error complete_code(const String &p_code, const String &p_base_path, Object *p_owner, List<String> *r_options, bool &r_force, String &r_call_hint) { return ERR_UNAVAILABLE; }
+
+    virtual Error lookup_code(const String &p_code, const String &p_symbol, const String &p_base_path, Object *p_owner, LookupResult &r_result) { return ERR_UNAVAILABLE; }
+
+    virtual void auto_indent_code(String &p_code, int p_from_line, int p_to_line) const;
+    virtual void add_global_constant(const StringName &p_variable, const Variant &p_value);
+
+    virtual void thread_enter();
+    virtual void thread_exit();
+
+    virtual String debug_get_error() const;
+    virtual int debug_get_stack_level_count() const;
+    virtual int debug_get_stack_level_line(int p_level) const;
+    virtual String debug_get_stack_level_function(int p_level) const;
+    virtual String debug_get_stack_level_source(int p_level) const;
+    virtual void debug_get_stack_level_locals(int p_level, List<String> *p_locals, List<Variant> *p_values, int p_max_subitems = -1, int p_max_depth = -1);
+    virtual void debug_get_stack_level_members(int p_level, List<String> *p_members, List<Variant> *p_values, int p_max_subitems = -1, int p_max_depth = -1);
+    virtual ScriptInstance *debug_get_stack_level_instance(int p_level) { return NULL; }
+    virtual void debug_get_globals(List<String> *p_globals, List<Variant> *p_values, int p_max_subitems = -1, int p_max_depth = -1);
+    virtual String debug_parse_stack_level_expression(int p_level, const String &p_expression, int p_max_subitems = -1, int p_max_depth = -1);
+
+    virtual Vector<StackInfo> debug_get_current_stack_info();
+
+    virtual void reload_all_scripts();
+    virtual void reload_tool_script(const Ref<Script> &p_script, bool p_soft_reload);
+
+    virtual void get_recognized_extensions(List<String> *p_extensions) const;
+    virtual void get_public_functions(List<MethodInfo> *p_functions) const;
+    virtual void get_public_constants(List<Pair<String, Variant> > *p_constants) const;
+
+    virtual void profiling_start();
+    virtual void profiling_stop();
+
+    virtual int profiling_get_accumulated_data(ProfilingInfo *p_info_arr, int p_info_max);
+    virtual int profiling_get_frame_data(ProfilingInfo *p_info_arr, int p_info_max);
+
+    virtual void *alloc_instance_binding_data(Object *p_object);
+    virtual void free_instance_binding_data(void *p_data);
+
+    virtual void frame();
+
+protected:
+
+    static void _bind_methods();
+
+private:
+
 };
--- register_types.cpp
+++ register_types.cpp
@@ -1,10 +1,57 @@
+/*
+ * This file is part of LuaScript
+ * https://github.com/perbone/luascrip/
+ *
+ * Copyright 2017-2018 Paulo Perbone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 
 #include "register_types.h"
 
 #include "luascript.h"
 
+LuaScriptLanguage *script_language = nullptr;
+//LuaScriptResourceFormatLoader *resource_loader = nullptr;
+//LuaScriptResourceFormatSaver *resource_saver = nullptr;
+
 void register_luascript_types() {
+
+    ClassDB::register_class<LuaScript>();
+
+    script_language = memnew(LuaScriptLanguage);
+    ScriptServer::register_language(script_language);
+
+//    resource_loader = memnew(LuaScriptResourceFormatLoader);
+//    ResourceLoader::add_resource_format_loader(resource_loader);
+
+//    resource_saver = memnew(LuaScriptResourceFormatSaver);
+//    ResourceSaver::add_resource_format_saver(resource_saver);
+
+#ifdef TOOLS_ENABLED
+//    ScriptEditor::register_create_syntax_highlighter_function(LuaSyntaxHighlighter::create);
+//    EditorNode::add_init_callback(_editor_init);
+#endif
 }
 
 void unregister_luascript_types() {
+
+    ScriptServer::unregister_language(script_language);
+
+    if (script_language)
+        memdelete(script_language);
+//    if (resource_loader)
+//        memdelete(resource_loader);
+//    if (resource_saver)
+//        memdelete(resource_saver);
 }
